6e65db818e8422033626f46b5cd62b8331453786
dkawasaki <daisuke.kawasaki@rakus-partners.co.jp>
2021-05-10 14:43:54 +0900

    LoginForm クラスの定義

---
+++ src/main/java/jp/co/sample/form/LoginForm.java
@@ -0,0 +1,26 @@
+package jp.co.sample.form;
+
+public class LoginForm {
+	
+	//メールアドレス
+	String mailAddress;
+	//パスワード
+	String password;
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "LoginForm [mailAddress=" + mailAddress + ", password=" + password + "]";
+	}
+
+}
