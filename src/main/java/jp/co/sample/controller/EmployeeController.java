19c3ac82fcb1603d82c27169f88f7e03097f175a
kaitoyamashita <kaito.yamashita@rakus-partners.co.jp>
2021-05-10 17:31:55 +0900

    EmployeeController

---
+++ src/main/java/jp/co/sample/controller/EmployeeController.java
@@ -0,0 +1,30 @@
+package jp.co.sample.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.domain.Employee;
+import jp.co.sample.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	@Autowired
+	private EmployeeService employeeService;
+	
+	/**
+	 * 従業員情報を取得
+	 * @param model
+	 * @return list.htmlへフォワード
+	 */
+	public String showList(Model model) {
+		List<Employee> allList=employeeService.showList();
+		model.addAttribute("employee",allList);
+		return "employee/list.html";
+	}
+}
