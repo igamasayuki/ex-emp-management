52441f0c0622ea2e124cb761746c9cba46da4b9e
kaitoyamashita <kaito.yamashita@rakus-partners.co.jp>
2021-05-10 14:22:17 +0900

    AdministratorService クラスにメソッドの追加

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.repository.AdministratorRepository;
 
 @Service
@@ -11,4 +12,12 @@ import jp.co.sample.repository.AdministratorRepository;
 public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
+	
+	/**
+	 * administratorRepositoryのinsert()メソッドを呼び出す
+	 * @param administrator
+	 */
+	public void insert(Administrator administrator) {
+		administratorRepository.insert(administrator);
+	}
 }
