1bd6819eac2f008d10232c2ba79258c1019e6382
kaitoyamashita <kaito.yamashita@rakus-partners.co.jp>
2021-05-10 12:10:11 +0900

    AdministratorController

---
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -0,0 +1,29 @@
+package jp.co.sample.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.service.AdministratorService;
+
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+	@Autowired
+	private AdministratorService administratorService;
+	
+	@ModelAttribute
+	public InsertAdministratorForm setUpInsertAdministratorForm() {
+		return new InsertAdministratorForm();
+	}
+	
+	/**
+	 * @return insert.htmlへフォワード
+	 */
+	@RequestMapping("/toInsert")
+	public String toInsert() {
+		return "administrator/insert.html";
+	}
+}
