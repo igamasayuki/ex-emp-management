912e990f0d2bf31789987932c988fdda523dd198
kaitoyamashita <kaito.yamashita@rakus-partners.co.jp>
2021-05-10 11:11:08 +0900

    AdministratorRepository

---
+++ src/main/java/jp/co/sample/repository/AdministratorRepository.java
@@ -0,0 +1,53 @@
+package jp.co.sample.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import jp.co.sample.domain.Administrator;
+
+@Repository
+public class AdministratorRepository {
+	
+	@Autowired
+	private NamedParameterJdbcTemplate template;
+	
+	/**
+	 * RowMapper定義
+	 * return administrator
+	 */
+	private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER=(rs,i)->{
+		Administrator administrator=new Administrator();
+		return administrator;
+	};
+	
+	/**
+	 * insertメソッドによる管理者情報挿入
+	 * @param administrator
+	 */
+	public void insert(Administrator administrator) {
+		Administrator admin=new Administrator();
+		admin=administrator;
+	}
+	/**
+	 * findbyメソッドによるメールアドレス・パスワード検索
+	 * @param mailAddress
+	 * @param password
+	 * @return
+	 */
+	public Administrator findByMailAddressAndPassword(String mailAddress,String password){
+		String sql = "SELECT id,name,mail_adderss,password FROM administrators WHERE mail_address=:mailAddress and password=:password";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("mailAddress",mailAddress).addValue("password",password);
+		List<Administrator> administratorList
+		 = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
+		if (administratorList.size() == 0) {
+		return null;
+		}
+		return administratorList.get(0);
+	}
+}
