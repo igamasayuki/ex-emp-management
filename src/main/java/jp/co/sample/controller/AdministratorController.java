660f109ae015f2ca96656dfbf5788d79c952c185
kaitoyamashita <kaito.yamashita@rakus-partners.co.jp>
2021-05-10 17:12:30 +0900

    ログイン確認

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,8 +1,13 @@
 package jp.co.sample.controller;
 
+import java.util.Objects;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,6 +21,9 @@ import jp.co.sample.service.AdministratorService;
 public class AdministratorController {
 	@Autowired
 	private AdministratorService administratorService;
+	
+	@Autowired
+	private HttpSession session;
 	
 	@ModelAttribute
 	public InsertAdministratorForm setUpInsertAdministratorForm() {
@@ -54,5 +62,21 @@ public class AdministratorController {
 	@RequestMapping("/")
 	public String toLogin() {
 		return "administrator/login.html";
+	}
+	
+	@RequestMapping("/login")
+	public String login(LoginForm form,Model model) {
+		Administrator administrator=new Administrator();
+		String mailAddress=form.getMailAddress();
+		String password=form.getPassword();
+		administrator=administratorService.login(mailAddress,password);
+		System.out.println(administrator);
+		if(Objects.equals(administrator,null)) {
+			model.addAttribute("message","メールアドレスまたはパスワードが不正です。");
+			return "forward:/";
+		}else {
+			session.setAttribute("administrator",administrator.getName());
+			return "forward:/employee/showList";
+		}
 	}
 }
