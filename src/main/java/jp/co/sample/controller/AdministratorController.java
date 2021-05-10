5be9de92b563b9371021251c00dd6d9e3c64d20b
dkawasaki <daisuke.kawasaki@rakus-partners.co.jp>
2021-05-10 13:49:56 +0900

    AdministratorController クラスにメソッドの追加

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,10 +1,12 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -32,6 +34,17 @@ public class AdministratorController {
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
+	}
+	
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm form) {
+		Administrator administrator = new Administrator();
+		//InsertAdministratorForm オブジェクトの中身を今イン スタンス化した Administrator ドメインオブジェクトに コピーする
+		BeanUtils.copyProperties(form, administrator);
+		//administratorService の insert()メソッドを呼ぶ
+		administratorService.insert(administrator);
+		//ログイン画面にリダイレクト
+		return "redirect:/";
 	}
 
 }
