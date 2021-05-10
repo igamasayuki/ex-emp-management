7d46727405dbda296d2aa1894c3fd537bc797705
dkawasaki <daisuke.kawasaki@rakus-partners.co.jp>
2021-05-10 12:04:29 +0900

    2.AdministratorServiceクラスと3.InsertAdministratorFormクラスを定義

---
+++ src/main/java/jp/co/sample/form/InsertAdministratorForm.java
@@ -0,0 +1,37 @@
+package jp.co.sample.form;
+
+public class InsertAdministratorForm {
+
+	//管理者名
+	String name;
+	//メールアドレス
+	String mailAddress;
+	//パスワード
+	String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMailAddress() {
+		return mailAddress;
+	}
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
+				+ "]";
+	}
+	
+	
+}
---
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -0,0 +1,15 @@
+package jp.co.sample.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import jp.co.sample.repository.AdministratorRepository;
+
+@Service
+@Transactional
+public class AdministratorService {
+	@Autowired
+	private AdministratorRepository administratorRepository;
+
+}
