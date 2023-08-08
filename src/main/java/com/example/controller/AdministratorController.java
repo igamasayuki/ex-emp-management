c8ade2e66fb9ccfbcda9f8b96a3c4a06d4bd4a52
ynagata <yngt2022@gmail.com>
2023-08-08 17:53:10 +0900

    管理者情報登録処理を実装

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 
@@ -22,5 +25,24 @@ public class AdministratorController {
     public String toInsert(InsertAdministratorForm insertAdministratorForm) {
         
         return "administrator/insert";
+    }
+
+    /**
+     * 管理者情報登録処理
+     * @param insertAdministratorForm 登録する管理者情報
+     * @return ログイン画面へリダイレクトする
+     */
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm insertAdministratorForm) {
+        // Administratorエンティティをインスタンス化
+        Administrator administrator = new Administrator();
+        // 受け取ったフォームパラメータをエンティティへコピーする
+        BeanUtils.copyProperties(insertAdministratorForm, administrator);
+
+        // 登録処理
+        administratorService.insert(administrator);
+
+        // ログイン画面へリダイレクトする
+        return "redirect:administrator/login";
     }
 }
