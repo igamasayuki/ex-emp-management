0cfc768e621fc82f326bf6debef337bbced6b6cf
ynagata <yngt2022@gmail.com>
2023-08-08 16:50:16 +0900

    管理者登録フォームクラス作成

---
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -0,0 +1,33 @@
+package com.example.form;
+
+public class InsertAdministratorForm {
+    /** 名前 */
+    private String name;
+    /** メールアドレス */
+    private String mailAddress;
+    /** パスワード */
+    private String password;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getMailAddress() {
+        return mailAddress;
+    }
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public String toString() {
+        return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
+                + "]";
+    }
+}
