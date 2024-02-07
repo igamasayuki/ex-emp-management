06eecd3f0e19b7369cea5fc5d0cc63837d4c369d
ayumitanaka <ayumi.tanaka@rakus-partners.co.jp>
2024-02-07 16:38:13 +0900

    AdministratorServiceクラスを追加

---
+++ src/main/java/com/example/service/AdministratorService.java
@@ -0,0 +1,16 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.repository.AdministratorRepository;
+
+@Service
+@Transactional
+public class AdministratorService {
+
+    @Autowired
+    private AdministratorRepository administratorRepository;
+
+}
