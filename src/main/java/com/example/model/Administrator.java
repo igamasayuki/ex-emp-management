4eb099ca584eb3580984015ded8b5243ad618f57
sugiurasaki <saki.sugiura@rakus-partners.co.jp>
2024-02-07 10:23:15 +0900

    Administratorクラス

---
+++ src/main/java/com/example/model/Administrator.java
@@ -0,0 +1,44 @@
+package com.example.model;
+
+public class Administrator {
+  /** ID */
+  Integer id;
+  /** 名前 */
+  String name;
+  /** メールアドレス */
+  String mailAddress;
+  /** パスワード */
+  String password;
+  public Integer getId() {
+    return id;
+  }
+  public void setId(Integer id) {
+    this.id = id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getMailAddress() {
+    return mailAddress;
+  }
+  public void setMailAddress(String mailAddress) {
+    this.mailAddress = mailAddress;
+  }
+  public String getPassword() {
+    return password;
+  }
+  public void setPassword(String password) {
+    this.password = password;
+  }
+  @Override
+  public String toString() {
+    return "Administrator [id=" + id + ", name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
+        + "]";
+  }
+
+  
+  
+}
