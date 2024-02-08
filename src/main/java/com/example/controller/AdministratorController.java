b912a1cff92d635b019c2d9bb7289ba1fcf946dd
ayumitanaka <ayumi.tanaka@rakus-partners.co.jp>
2024-02-08 15:46:03 +0900

    AdministratorControllerの修正 - iinsertメソッドの実装.

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -3,9 +3,11 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.form.InsertAdministratorForm;
+import com.example.model.Administrator;
 import com.example.service.AdministratorService;
 
 @Controller
@@ -25,6 +27,25 @@ public class AdministratorController {
     @GetMapping({ "/toInsert", "/toInsert/" })
     public String toInsert(InsertAdministratorForm form) {
         return "administrator/insert";
+
+    }
+
+    /**
+     * 管理者情報を登録する.
+     * 
+     * @param form
+     * @return
+     */
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form) {
+        Administrator admin = new Administrator();
+        admin.setName(form.getName());
+        admin.setMailAddress(form.getMailAddress());
+        admin.setPassword(form.getPassword());
+
+        service.insert(admin);
+
+        return "redirect:/";
 
     }
 }
