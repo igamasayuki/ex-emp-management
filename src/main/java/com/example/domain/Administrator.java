48dfc6ad4ed131a35c33c5c4e36a33b7fb911249
guiqi97@gmail.com <guiqi97@gmail.com>
2024-02-08 17:23:10 +0900

    Administrator.javaの内容完成、Employee.javaを修正

--- src/main/java/com/example/domain/Administrator.java
+++ src/main/java/com/example/domain/Administrator.java
@@ -1,5 +1,48 @@
 package com.example.domain;
 
 public class Administrator {
+    private Integer id;
+    private String name;
+    private String mailAddress;
+    private String password;
+
+    public Administrator() {
+    }
     
+    public Administrator(Integer id, String name, String mailAddress, String password) {
+        this.id = id;
+        this.name = name;
+        this.mailAddress = mailAddress;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getMailAddress() {
+        return mailAddress;
+    }
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    @Override
+    public String toString() {
+        return "Administrator [id=" + id + ", name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
+                + "]";
+    }
 }
--- src/main/java/com/example/domain/Employee.java
+++ src/main/java/com/example/domain/Employee.java
@@ -1,20 +1,41 @@
 package com.example.domain;
 
+import java.util.Date;
+
 public class Employee {
     private Integer id;
     private String name;
+    private String image;
+    private String gender;
+    private Date hireDate;
     private String mailAddress;
-    private String password;
+    private String zipCode;
+    private String address;
+    private String telephone;
+    private Integer salary;
+    private String characteristics;
+    private Integer dependentsCount;
     
     public Employee() {
     }
     
-    public Employee(Integer id, String name, String mailAddress, String password) {
+    public Employee(Integer id, String name, String image, String gender, Date hireDate, String mailAddress,
+            String zipCode, String address, String telephone, Integer salary, String characteristics,
+            Integer dependentsCount) {
         this.id = id;
         this.name = name;
+        this.image = image;
+        this.gender = gender;
+        this.hireDate = hireDate;
         this.mailAddress = mailAddress;
-        this.password = password;
+        this.zipCode = zipCode;
+        this.address = address;
+        this.telephone = telephone;
+        this.salary = salary;
+        this.characteristics = characteristics;
+        this.dependentsCount = dependentsCount;
     }
+
     public Integer getId() {
         return id;
     }
@@ -27,21 +48,73 @@ public class Employee {
     public void setName(String name) {
         this.name = name;
     }
+    public String getImage() {
+        return image;
+    }
+    public void setImage(String image) {
+        this.image = image;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public Date getHireDate() {
+        return hireDate;
+    }
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
     public String getMailAddress() {
         return mailAddress;
     }
     public void setMailAddress(String mailAddress) {
         this.mailAddress = mailAddress;
     }
-    public String getPassword() {
-        return password;
+    public String getZipCode() {
+        return zipCode;
     }
-    public void setPassword(String password) {
-        this.password = password;
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
     }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getTelephone() {
+        return telephone;
+    }
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+    public Integer getSalary() {
+        return salary;
+    }
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+    public String getCharacteristics() {
+        return characteristics;
+    }
+    public void setCharacteristics(String characteristics) {
+        this.characteristics = characteristics;
+    }
+    public Integer getDependentsCount() {
+        return dependentsCount;
+    }
+    public void setDependentsCount(Integer dependentsCount) {
+        this.dependentsCount = dependentsCount;
+    }
+
     @Override
     public String toString() {
-        return "Employee [id=" + id + ", name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
-                + "]";
+        return "Employee [id=" + id + ", name=" + name + ", image=" + image + ", gender=" + gender + ", hireDate="
+                + hireDate + ", mailAddress=" + mailAddress + ", zipCode=" + zipCode + ", address=" + address
+                + ", telephone=" + telephone + ", salary=" + salary + ", characteristics=" + characteristics
+                + ", dependentsCount=" + dependentsCount + "]";
     }
+    
 }
