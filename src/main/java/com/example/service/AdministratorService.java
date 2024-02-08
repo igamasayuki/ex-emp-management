720989b6310b3001bab38b0c35fefedb41f943be
ayumitanaka <ayumi.tanaka@rakus-partners.co.jp>
2024-02-08 15:37:08 +0900

    AdministratorServiceクラスを修正 - insert()メソッドを実装.

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.model.Administrator;
 import com.example.repository.AdministratorRepository;
 
 @Service
@@ -12,5 +13,14 @@ public class AdministratorService {
 
     @Autowired
     private AdministratorRepository administratorRepository;
+
+    /**
+     * 管理者情報を挿入する.
+     * 
+     * @param administrator
+     */
+    public void insert(Administrator administrator) {
+        administratorRepository.insert(administrator);
+    }
 
 }
