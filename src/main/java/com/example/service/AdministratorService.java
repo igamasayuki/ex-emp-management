ccc1424d14154f8238e1c6aee72ea7062e26f883
takahiro <taka.araat32@gmail.com>
2024-11-12 12:06:53 +0900

    administratorServiceクラス修正(管理者情報挿入機能)

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 /**
@@ -18,5 +19,15 @@ public class AdministratorService {
     /** administratorRepository */
     @Autowired
     private AdministratorRepository administratorRepository;
+
+    /**
+     * 管理者情報を挿入する
+     *
+     * @param administrator 管理者情報
+     */
+    public void insert(Administrator administrator) {
+
+        administratorRepository.insert(administrator);
+    }
 
 }
