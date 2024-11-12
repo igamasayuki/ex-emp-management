354759bde6c36c7becdb4066232f1037d929fc49
takahiro <taka.araat32@gmail.com>
2024-11-12 13:49:08 +0900

    EmployeeServiceクラス作成

---
+++ src/main/java/com/example/service/EmployeeService.java
@@ -0,0 +1,33 @@
+package com.example.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+
+/**
+ * 管理者関連機能の業務処理を行うサービス
+ *
+ * @author T.Araki
+ */
+@Service
+@Transactional
+public class EmployeeService {
+
+    /** employeeRepository */
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    /**
+     * 従業員情報を全件取得
+     *
+     * @return 従業員情報リスト
+     */
+    public List<Employee> showList() {
+        return employeeRepository.findAll();
+    }
+}
