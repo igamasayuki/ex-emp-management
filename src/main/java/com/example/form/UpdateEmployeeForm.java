62bf24dd9743e3f3e2fcd5308ec7ddb2f5be320c
Sora_Kanamaru <sora.kanamaru@rakus-partners.co.jp>
2024-11-12 14:47:02 +0900

    UpdateEmployeeForm クラスの定義

--- src/main/java/com/example/form/UpdateEmployeeForm.java
+++ src/main/java/com/example/form/UpdateEmployeeForm.java
@@ -2,4 +2,28 @@ package com.example.form;
 
 public class UpdateEmployeeForm {
 
+    private String id;
+    private String dependentsCount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDependentsCount() {
+        return dependentsCount;
+    }
+
+    public void setDependentsCount(String dependentsCount) {
+        this.dependentsCount = dependentsCount;
+    }
+
+    @Override
+    public String toString() {
+        return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+    }
+
 }
