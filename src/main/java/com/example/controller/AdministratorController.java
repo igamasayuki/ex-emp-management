f11e4364f4da6075a048af36b205debb0ea5ed08
a.moeka <keywnm@gmail.com>
2024-11-12 13:50:07 +0900

    AdministratorControllerの編集(toLoginメソッドの追加)

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
+import com.example.form.LoginForm;
 import com.example.service.AdministratorService;
 
 /**
@@ -49,5 +50,16 @@ public class AdministratorController {
     administratorService.insert(administrator);
 
     return "redirect:/";
+  }
+
+  /**
+   * ログイン画面を表示する.
+   * 
+   * @param form
+   * @return ログイン画面
+   */
+  @GetMapping("/")
+  public String toLogin(LoginForm form) {
+    return "administrator/login";
   }
 }
