76ad4d902f6877922c7cd86d1b4b6ec679b5dd12
takahiro <taka.araat32@gmail.com>
2024-11-12 13:10:04 +0900

    ログイン処理追加

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -30,4 +30,15 @@ public class AdministratorService {
         administratorRepository.insert(administrator);
     }
 
+    /**
+     * ログイン処理をする
+     *
+     * @param mailAddress メールアドレス
+     * @param password    パスワード
+     * @return 管理者情報
+     */
+    public Administrator login(String mailAddress, String password) {
+        return administratorRepository.findByMailAddressPassword(mailAddress, password);
+    }
+
 }
