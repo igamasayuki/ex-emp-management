d4c59e94142b007e9cc126a88a2b2ec23f9f2370
a.moeka <keywnm@gmail.com>
2024-11-12 15:47:51 +0900

    EmployeeControllerの作成

---
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.example.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Employee;
+import com.example.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+  @Autowired
+  private EmployeeService employeeService;
+
+  /**
+   * 
+   * @param model
+   * @return 従業員一覧画面
+   */
+  @GetMapping("/showList")
+  public String showList(Model model) {
+    List<Employee> employeeList = new ArrayList<>();
+    employeeList = employeeService.showList();
+
+    model.addAttribute("employeeList", employeeList);
+
+    return "employee/list";
+  }
+}
