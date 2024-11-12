084281bce42d34d7543dea85721a787be56f5920
Sora_Kanamaru <sora.kanamaru@rakus-partners.co.jp>
2024-11-12 11:30:22 +0900

    コントローラークラスの作成

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -2,4 +2,32 @@ package com.example.form;
 
 public class InsertAdministratorForm {
 
+    private String name;
+    private String mailAddress;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
