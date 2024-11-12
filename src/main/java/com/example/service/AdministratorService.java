651a97c5a7f2a6416dc876269d614c237597ca70
yutaka-yutaka <arwirn@gmail.com>
2024-11-12 14:21:36 +0900

    w

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 @Service
@@ -12,6 +13,10 @@ public class AdministratorService {
 
     @Autowired
     private AdministratorRepository administratorRepository;
+
+    public void insert(Administrator administrator){
+        
+    }
 
     
 
