35474751def9347416c681a6066ea1e62fc4dd84
harasawa <h_silvermitt@outlook.jp>
2024-11-12 09:57:17 +0900

    init

---
+++ src/domain/Employee.java
@@ -0,0 +1,13 @@
+public class Employee {
+    private Integer id;
+    private String name;
+    private String image;
+    private String gender;
+    private java.util.Date hireDate;
+    private String mailAddress;
+    private String zipCode;
+    private String address;
+    private String telephone;
+    private Integer salary;
+    private String 
+}
