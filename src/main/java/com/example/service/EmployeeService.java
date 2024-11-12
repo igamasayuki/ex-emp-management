63e4160f5288cfbdab97a2e1d68560e125122257
a.moeka <keywnm@gmail.com>
2024-11-12 17:38:13 +0900

    EmployeeServiceの編集(updateメソッドの追加)

--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -39,4 +39,13 @@ public class EmployeeService {
   public Employee showDetail(Integer id) {
     return employeeRepository.load(id);
   }
+
+  /**
+   * 従業員情報を更新する.
+   * 
+   * @param employee
+   */
+  public void update(Employee employee) {
+    employeeRepository.update(employee);
+  }
 }
