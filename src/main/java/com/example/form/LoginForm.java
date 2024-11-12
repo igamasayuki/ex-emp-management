456ece372ba2fd2250642e1314706918444a3f23
yutaka-yutaka <arwirn@gmail.com>
2024-11-12 15:16:56 +0900

    loginform

---
+++ src/main/java/com/example/form/LoginForm.java
@@ -0,0 +1,30 @@
+package com.example.form;
+
+public class LoginForm {
+    // メールアドレス
+    private String mailAddress;
+    // パスワード
+    private String password;
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginForm [mailAddress=" + mailAddress + ", password=" + password + "]";
+    }
+
+}
