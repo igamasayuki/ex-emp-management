5260b6d1fbcee05b85d793e37898c7500da57c84
Sora_Kanamaru <sora.kanamaru@rakus-partners.co.jp>
2024-11-12 14:44:27 +0900

    EmployeeService クラスの定義

--- src/main/java/com/example/service/EmployeeService.java
+++ src/main/java/com/example/service/EmployeeService.java
@@ -1,5 +1,35 @@
 package com.example.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.domain.Employee;
+import com.example.repository.EmployeeRepository;
+
+/**
+ * @author 金丸天
+ *         Employeeのサービスクラス
+ */
+@Service
+@Transactional
 public class EmployeeService {
+    /**
+     * リポジトリのオブジェクトを注入
+     */
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    /**
+     * リポジトリの全件検索の呼び出し
+     * 
+     * @return すべての従業員のリスト
+     */
+    public List<Employee> showList() {
+        List<Employee> employeeList = employeeRepository.findAll();
+        return employeeList;
+    }
 
 }
