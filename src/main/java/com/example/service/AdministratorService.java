ce2a08a036d1638def6552bbfa27413b47c9e9f7
a.moeka <keywnm@gmail.com>
2024-11-12 13:07:07 +0900

    AdminstratorServiceの編集(insertメソッドの追加)

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 
 /**
@@ -18,4 +19,8 @@ public class AdministratorService {
 
   @Autowired
   private AdministratorRepository administratorRepository;
+
+  private void insert(Administrator administrator) {
+    administratorRepository.insert(administrator);
+  }
 }
