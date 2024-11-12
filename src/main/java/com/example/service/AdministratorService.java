82aa23a43199559937b9bea3db0b41266baff37b
harasawa <h_silvermitt@outlook.jp>
2024-11-12 16:51:34 +0900

    一時保存

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.domain.Administrator;
 import com.example.domain.Employee;
 import com.example.repository.AdministratorRepository;
 
@@ -14,6 +15,10 @@ import com.example.repository.AdministratorRepository;
 public class AdministratorService {
     @Autowired
     private AdministratorRepository repository;
+
+    // public void insert(Administrator administrato) {
+    //     return repository.insert(administrato);
+    // }
 
     // public List<Employee> findAll() {
     //     return repository.findAll();
