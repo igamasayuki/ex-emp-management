401bc527f378365e578694ab1ad5e31c9e0c9814
a.moeka <keywnm@gmail.com>
2024-11-12 14:00:57 +0900

    AdministratorServiceの編集(loginメソッドの追加)

--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -20,7 +20,22 @@ public class AdministratorService {
   @Autowired
   private AdministratorRepository administratorRepository;
 
+  /**
+   * 管理者情報登録をする業務処理を行う.
+   * 
+   * @param administrator
+   */
   public void insert(Administrator administrator) {
     administratorRepository.insert(administrator);
+  }
+
+  /**
+   * ログインをする業務処理を行う.
+   * 
+   * @param mailAddress, password
+   * @return administrator
+   */
+  public Administrator login(String mailAddress, String password) {
+    return administratorRepository.findByMailAddressAndPassword(mailAddress, password);
   }
 }
