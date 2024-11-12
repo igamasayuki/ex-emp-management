aa28d3a8c11f4f431e986659db8d40534fff7cb2
Sora_Kanamaru <sora.kanamaru@rakus-partners.co.jp>
2024-11-12 16:43:28 +0900

    EmployeeController クラスにメソッドの追加

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.domain.Employee;
@@ -49,6 +50,16 @@ public class EmployeeController {
         model.addAttribute("employee", employee);
 
         return "employee/detail.html";
+
+    }
+
+    @PostMapping("/update")
+    public String update(UpdateEmployeeForm form) {
+        Integer EmId = Integer.parseInt(form.getId());
+        Employee employee = employeeService.showDetail(EmId);
+        employee.setDependentsCount(Integer.parseInt(form.getDependentsCount()));
+        employeeService.update(employee);
+        return "redirect:/employee/showList";
 
     }
 
