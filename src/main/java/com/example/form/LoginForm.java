8790996bee03a8fccbc9f5475b07f9ad389ca9b0
a.moeka <keywnm@gmail.com>
2024-11-12 13:38:14 +0900

    LoginFormの作成

---
+++ src/main/java/com/example/form/LoginForm.java
@@ -0,0 +1,36 @@
+package com.example.form;
+
+/**
+ * ログイン時に使用するフォーム.
+ * 
+ * @author M.aoki
+ */
+public class LoginForm {
+  /** メールアドレス */
+  private String mailAddress;
+
+  /** パスワード */
+  private String password;
+
+  public String getMailAddress() {
+    return mailAddress;
+  }
+
+  public void setMailAddress(String mailAddress) {
+    this.mailAddress = mailAddress;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String toString() {
+    return "LoginForm [mailAddress=" + mailAddress + ", password=" + password + "]";
+  }
+
+}
