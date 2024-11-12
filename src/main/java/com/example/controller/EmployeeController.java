a17d78213865f22b665c6336facd36539527d651
takahiro <taka.araat32@gmail.com>
2024-11-12 15:12:32 +0900

    従業員情報更新処理追加

--- src/main/java/com/example/controller/EmployeeController.java
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -5,8 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Employee;
 import com.example.form.UpdateEmployeeForm;
 import com.example.service.EmployeeService;
 
@@ -51,6 +53,20 @@ public class EmployeeController {
     public String showDetail(String id, Model model, UpdateEmployeeForm form) {
         model.addAttribute("employee", employeeService.showDetail(Integer.parseInt(id)));
         return "employee/detail";
+    }
+
+    /**
+     * 従業員詳細を更新する
+     *
+     * @param form 従業員更新フォーム
+     * @return 従業員一覧画面(リダイレクト)
+     */
+    @PostMapping("/update")
+    public String update(UpdateEmployeeForm form) {
+        Employee employee = employeeService.showDetail(Integer.parseInt(form.getId()));
+        employee.setDependentsCount(Integer.parseInt(form.getDependentsCount()));
+        employeeService.update(employee);
+        return "redirect:/employee/showList";
     }
 
 }
