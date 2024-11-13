977bbae747475928222fd1636232ed17d898901a
a.moeka <keywnm@gmail.com>
2024-11-13 11:42:27 +0900

    AdministratorControllerの編集(insertメソッドをバリデーションに対応するように修正)

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,12 +1,17 @@
 package com.example.controller;
 
+import java.sql.ResultSet;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
@@ -37,7 +42,9 @@ public class AdministratorController {
    * @return 管理者情報登録画面
    */
   @GetMapping("/toInsert")
-  public String toInsert(InsertAdministratorForm form) {
+  public String toInsert(
+      InsertAdministratorForm form,
+      Model model) {
     return "administrator/insert";
   }
 
@@ -48,7 +55,16 @@ public class AdministratorController {
    * @return ログイン画面
    */
   @PostMapping("/insert")
-  public String insert(InsertAdministratorForm form) {
+  public String insert(
+      @Validated InsertAdministratorForm form,
+      BindingResult result,
+      RedirectAttributes redirectAttributes,
+      Model model) {
+
+    if (result.hasErrors()) {
+      return toInsert(form, model);
+    }
+
     Administrator administrator = new Administrator();
 
     BeanUtils.copyProperties(form, administrator);
