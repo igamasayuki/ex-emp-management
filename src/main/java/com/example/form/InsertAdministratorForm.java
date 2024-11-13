ab1b69da37e90f24784eccaff190cf226b317c4c
a.moeka <keywnm@gmail.com>
2024-11-13 11:31:37 +0900

    InsertAdministratorFormの編集(バリデーションの設定)

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -1,5 +1,10 @@
 package com.example.form;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 /**
  * 管理者情報登録時に使用するフォーム
  * 
@@ -7,12 +12,17 @@ package com.example.form;
  */
 public class InsertAdministratorForm {
   /** 名前 */
+  @NotBlank(message = "名前を入力してください")
   private String name;
 
   /** メールアドレス */
+  @Size(min = 1, max = 48, message = "Eメールは1文字以上48文字以内で入力してください")
+  @Email(message = "Eメールの形式が不正です")
   private String mailAddress;
 
   /** パスワード */
+  @Size(min = 1, max = 48, message = "パスワードは1文字以上48文字で入力してください")
+  @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$", message = "パスワードは英字と数字を両方含む必要があります")
   private String password;
 
   public String getName() {
