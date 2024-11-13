e20c5b383f1f301d351be2275cfcb4d7037e451c
a.moeka <keywnm@gmail.com>
2024-11-13 16:41:28 +0900

    バリデーション修正

--- src/main/java/com/example/form/UpdateEmployeeForm.java
+++ src/main/java/com/example/form/UpdateEmployeeForm.java
@@ -1,6 +1,6 @@
 package com.example.form;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 /**
@@ -13,7 +13,7 @@ public class UpdateEmployeeForm {
   private String id;
 
   /** 扶養人数 */
-  @NotNull(message = "扶養人数を入力してください")
+  @NotBlank(message = "扶養人数を入力してください")
   @Pattern(regexp = "^\\d+$", message = "半角数値で入力してください")
   private String dependentsCount;
 
