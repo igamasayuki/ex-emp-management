52a103a82fda1ad3e945041dc2c532a9d58409dc
a.moeka <keywnm@gmail.com>
2024-11-13 11:47:21 +0900

    insert.htmlの編集(バリデーションに引っかかった時にエラーメッセージが表示されるよう修正)

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,7 +1,5 @@
 package com.example.controller;
 
-import java.sql.ResultSet;
-
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
