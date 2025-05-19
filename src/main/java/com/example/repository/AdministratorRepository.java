dce6bbd0e3581ac8e309aaf65ef9da21de708336
murosyo01 <murozuka75@gmail.com>
2025-05-19 17:31:03 +0900

    insertメソッドの実装

--- src/main/java/com/example/repository/AdministratorRepository.java
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -1,4 +1,31 @@
 package com.example.repository;
 
+import com.example.domain.Administrator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AdministratorRepository {
+    private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER = (rs, i) -> {
+        Administrator administrator = new Administrator();
+        administrator.setId(rs.getInt("id"));
+        administrator.setName(rs.getString("name"));
+        administrator.setMailAddress(rs.getString("mail_address"));
+        administrator.setPassword(rs.getString("password"));
+        return administrator;
+    };
+
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    public void insert(Administrator administrator){
+        SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
+
+        String sql = "INSET INTO administrator (name, mail_address, password) VALUES (name = :name, mail_address = :mailAddress, password = :password)";
+        template.update(sql, param);
+    }
 }
