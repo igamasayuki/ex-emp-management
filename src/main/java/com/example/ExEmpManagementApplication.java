8740462cd7d021ff3acbc406e66c02033d071e44
igamasayuki <igamasayuki@gmail.com>
2025-05-19 16:46:33 +0900

    SpringBootのバージョン、JDK、Geradleアップデート

--- src/main/java/com/example/ExEmpManagementApplication.java
+++ src/main/java/com/example/ExEmpManagementApplication.java
@@ -2,12 +2,18 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExEmpManagementApplication {
+public class ExEmpManagementApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExEmpManagementApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ExEmpManagementApplication.class);
+	}
 }
