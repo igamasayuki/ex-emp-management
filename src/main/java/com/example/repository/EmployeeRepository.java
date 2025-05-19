8a8bb1faeecf5291efaa5923448ff3365a65631a
murosyo01 <murozuka75@gmail.com>
2025-05-20 08:54:17 +0900

    findByIdメソッドを作成

--- src/main/java/com/example/repository/EmployeeRepository.java
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -1,12 +1,16 @@
 package com.example.repository;
 
 import com.example.domain.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.Iterator;
 import java.util.List;
 
 @Repository
@@ -29,6 +33,7 @@ public class EmployeeRepository {
         return employee;
     };
 
+    @Autowired
     private NamedParameterJdbcTemplate template;
 
     public List<Employee> findAll(){
@@ -39,6 +44,15 @@ public class EmployeeRepository {
             return null;
         }
         return employeeList;
+    }
+
+    public Employee findById(Integer id){
+        String sql = "SELECT * FROM employees WHERE id = :id;";
+        SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+
+        Employee employee = template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
+
+        return employee;
     }
 
 }
