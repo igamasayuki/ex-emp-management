0f1e41511b68a413bbff633527bdeb50026454f2
murosyo01 <murozuka75@gmail.com>
2025-05-20 09:58:54 +0900

    toLoginメソッドの作成

--- src/main/java/com/example/controller/AdministatorController.java
+++ src/main/java/com/example/controller/AdministatorController.java
@@ -1,4 +1,28 @@
 package com.example.controller;
 
+import com.example.domain.Administrator;
+import com.example.form.InsertAdministratorForm;
+import com.example.form.LoginForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/administrator")
 public class AdministatorController {
+
+    @GetMapping("/")
+    public String toLogin(LoginForm form){
+        return "login";
+    }
+
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form){
+        Administrator administrator = new Administrator();
+//        administrator.setName();
+        return "redirect:/administrator/";
+    }
+
 }
