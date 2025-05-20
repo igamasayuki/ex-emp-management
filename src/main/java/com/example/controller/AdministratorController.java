722dbd3e431f29962b4c63b0da3ef570b381cb2c
Kazuuma-19 <kazu15870@gmail.com>
2025-05-20 10:42:28 +0900

    feat: 管理者の登録処理を実装

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,11 +1,13 @@
 package com.example.controller;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,9 +16,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdministratorController {
     private final AdministratorService administratorService;
 
+    @GetMapping
+    public String toLogin() {
+        return "administrator/login";
+    }
+
+    /**
+     * 管理者情報をメールアドレスとパスワードで検索する
+     * @param insertAdministratorForm 登録する管理者情報
+     * @param model リクエストスコープ
+     * @return リダイレクト先のURL
+     * @author kazuma.saito
+     */
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm insertAdministratorForm, Model model) {
         model.addAttribute("administratorForm", insertAdministratorForm);
         return "administrator/insert";
+    }
+
+    /**
+     * 管理者情報を登録する
+     * @param insertAdministratorForm 登録する管理者情報
+     * @param model リクエストスコープ
+     * @return リダイレクト先のURL
+     * @author kazuma.saito
+     */
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm insertAdministratorForm, Model model) {
+        Administrator administrator = new Administrator();
+        administrator.setName(insertAdministratorForm.getName());
+        administrator.setMailAddress(insertAdministratorForm.getMailAddress());
+        administrator.setPassword(insertAdministratorForm.getPassword());
+
+        administratorService.insert(administrator);
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/service/AdministratorService.java
+++ src/main/java/com/example/service/AdministratorService.java
@@ -1,5 +1,6 @@
 package com.example.service;
 
+import com.example.domain.Administrator;
 import com.example.repository.AdministratorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,4 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AdministratorService {
     private final AdministratorRepository administratorRepository;
+
+    /**
+     * 管理者情報を登録する
+     *
+     * @param administrator 登録する管理者情報
+     */
+    public void insert(Administrator administrator) {
+        administratorRepository.insert(administrator);
+    }
 }
