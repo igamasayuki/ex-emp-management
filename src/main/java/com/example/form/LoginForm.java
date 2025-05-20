d665c537522a2254fc63802aa641bc289f84e6e1
murosyo01 <murozuka75@gmail.com>
2025-05-20 10:04:40 +0900

    LoginForm作成

--- src/main/java/com/example/form/LoginForm.java
+++ src/main/java/com/example/form/LoginForm.java
@@ -1,4 +1,27 @@
 package com.example.form;
 
 public class LoginForm {
+    private String mailAddress;
+    private String password;
+
+    public String getMailAddress() {
+        return mailAddress;
+    }
+
+    public void setMailAddress(String mailAddress) {
+        this.mailAddress = mailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString(){
+        return "LoginInfo ［ mailAddress = " + mailAddress + ", password = " + password + " ］";
+    }
 }
