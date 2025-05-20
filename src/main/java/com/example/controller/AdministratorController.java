fd17eb1d4c5ddc8d803aa5f6b0b4c7996553a57a
Y-Kanekoo <kaneko.yuki2001@gmail.con>
2025-05-20 16:44:12 +0900

    管理者情報を登録する機能 作成

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,13 @@
 package com.example.controller;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -24,13 +27,32 @@ public class AdministratorController {
     }
 
     /**
-     * 管理者登録画面を出力します
+     * 管理者登録画面を出力します.
+     *
      * @param form　フォーム
      * @return 管理者画面
      */
     @GetMapping("/toInsert")
     public String toInsert(InsertAdministratorForm form){
         return "administrator/insert";
+    }
+
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form){
+
+        Administrator administrator = new Administrator();
+
+//        手動で設定
+//        administrator.setName(form.getName());
+//        administrator.setPassword(form.getPassword());
+//        administrator.setMailAddress(form.getMailAddress());
+
+        //自動で設定
+        BeanUtils.copyProperties(form, administrator);
+
+        administratorService.insert(administrator);
+        return "redirect:/";
+
     }
 
 
